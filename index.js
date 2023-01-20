195fee029e03ac925baded6c7e0f41faa2a77e3e
ericndungutse <dav.ndungutse@gmail.com>
2023-01-20 20:33:11 +0200

    connect mongodb with mongoose

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv").config({ path: "./config.env" });
 
 const app = require("./app");
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.DATABASE_LOCAL, {}, () => {
+  console.log("********************************");
+  console.log("Database Connected Successfully!");
+  console.log("********************************");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running at ${port}...`));
