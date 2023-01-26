dd483ec155c88a5ad8b219ce78448dcfd6b8092d
ericndungutse <dav.ndungutse@gmail.com>
2023-01-26 09:39:31 +0200

    feature(documentation): add documentation about API

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -7,10 +7,107 @@ const {
 } = require("../controller/messages.controller");
 const { protect, restrictTo } = require("../middlewares/authorization");
 
-router
-  .route("/")
-  .get(protect, restrictTo("sdmin"), getMessages)
-  .post(createMessage);
-router.route("/:id").get(protect, restrictTo("admin"), getMessage);
+/**
+ *@swagger
+ * /api/messages:
+ *  post:
+ *      tags:
+ *       - Messages
+ *      summary: Create a new message
+ *      description: Create a new message
+ *      requestBody:
+ *        description: Create a new message
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                name:
+ *                 type: string
+ *                 example: John Mukamba
+ *                email:
+ *                 type: string
+ *                 example: 'name@example.com'
+ *                message:
+ *                 type: string
+ *                 example: 'Message'
+ *        required: true
+ *      responses:
+ *        '201':
+ *          description: Created
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                 status:
+ *                  type: string
+ *                  example: success
+ *                 data:
+ *                  type: object
+ *                  properties:
+ *                   blog:
+ *                    type: object
+ *                    properties:
+ *                      _id:
+ *                       type: string
+ *                       example: 4h67dsbew7wbre87
+ *                      name:
+ *                       type: string
+ *                       example: Eric Ndungutse
+ *                      email:
+ *                       type: string
+ *                       example: 'name@example.com'
+ *                      message:
+ *                       type: string
+ *                       example: Message
+ */
+router.route("/").post(createMessage);
+router.use(protect);
+/**d
+ * @swagger
+ * /api/messages:
+ *  get:
+ *   summary: For getting all messages
+ *   tags: [Messages]
+ *
+ *   description: This is used to get all messages.
+ *
+ *   responses:
+ *      200:
+ *       description: Returned messages
+ *       content:
+ *        application/json:
+ *         schema:
+ *          type: array
+ */
+router.route("/").get(restrictTo("sdmin"), getMessages);
+
+/**
+ * @swagger
+ * /api/messages/{id}:
+ *  get:
+ *   summary: Get message by ID
+ *   tags: [Messages]
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *        type: string
+ *        required: true
+ *      description: alphanumerical ID of the message to get
+ * 
+ *   description: This is used to get single message by its ID.
+ *
+ *   responses:
+ *      200:
+ *       description: Returned message
+ *       content:
+ *        application/json:
+ *         schema:
+ *          type: object
+
+ */
+router.route("/:id").get(restrictTo("admin"), getMessage);
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,80 @@ const router = require("express").Router();
 const { protect } = require("../middlewares/authorization");
 const { updateMe, getAllUsers } = require("../controller/user.controller");
 
+/**
+ *@swagger
+ * /api/users:
+ *  post:
+ *      tags:
+ *       - Users
+ *      summary: Add a new user
+ *      description: Add a new user
+ *      requestBody:
+ *        description: Create a new user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                name:
+ *                 type: string
+ *                 example: John Mukamba
+ *                email:
+ *                 type: string
+ *                 example: john@email.com
+ *                password:
+ *                 type: string
+ *                 example: '12345'
+ *                confirmPassword:
+ *                 type: string
+ *                 example: '12345'
+ *        required: true
+ *      responses:
+ *        '200':
+ *          description: Successful operation
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                 status:
+ *                  type: string
+ *                  example: success
+ *                 data:
+ *                  type: object
+ *                  properties:
+ *                   user:
+ *                    type: object
+ *                    properties:
+ *                      _id:
+ *                       type: string
+ *                       example: 4h67dsbew7wbre87
+ *                      name:
+ *                       type: string
+ *                       example: John Mukamba
+ *                      email:
+ *                       type: string
+ *                       example: john@email.com
+ */
 router.patch("/updateMe", protect, updateMe);
+
+/**
+ * @swagger
+ * /api/users:
+ *  get:
+ *   summary: For getting all users
+ *   tags: [Users]
+ *
+ *   description: This is used to get all users of the application
+ *
+ *   responses:
+ *      200:
+ *       description: Returned users
+ *       content:
+ *        application/json:
+ *         schema:
+ *          type: array
+ */
 router.route("/").get(getAllUsers);
 
 module.exports = router;
