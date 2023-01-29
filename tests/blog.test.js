fc78757f2620860d6114fc1aa34aafaadd35bc43
Eric Tuyizere NDUNGUTSE <55640083+ericndungutse@users.noreply.github.com>
2023-01-29 15:32:13 +0200

    chore(testing): add tests for getting comments and likes on blogs (#15)
    - ensure that getting comments and likes of blogs on nested routes work
    - implement testing on nested routes of blogs
    
    [Finishes #48-testing-blog]
    Please enter the commit message for your changes. Lines starting

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -3,25 +3,18 @@ const mongoose = require("mongoose");
 const app = require("../app");
 
 /* Connecting to the database before each test. */
-beforeEach(async () => {
+beforeAll(async () => {
+  mongoose.set("strictQuery", true);
   await mongoose.connect("mongodb://localhost:27017/eric_ndungutse_test");
 });
 
 /* Closing database connection after each test. */
-afterEach(async () => {
+afterAll(async () => {
+  await mongoose.disconnect();
   await mongoose.connection.close();
 });
 
 describe("CRUD Blog Test", () => {
-  // Creating New Blog
-  test("Should create a blog", async () => {
-    const res = await request(app).post("/api/blogs").send({
-      title: "New Blog",
-    });
-
-    expect(res.body.status).toBe("fail");
-  }, 50000);
-
   // Getting All Blogs
   test("Should return all blogs", async () => {
     const res = await request(app).get("/api/blogs");
@@ -34,4 +27,32 @@ describe("CRUD Blog Test", () => {
     expect(res.body.status).toBe("success");
     expect(res.body.data.blog._id).toBe("63d3e1635d04138e68e53c90");
   });
+
+  // Getting Comments on blog
+  it("should return comments of blog with route /blogs/blogId/comments", async () => {
+    const res = await request(app).get(
+      "/api/blogs/63d3e1635d04138e68e53c90/comments"
+    );
+    expect(res.body.status).toBe("success");
+  });
+
+  // Getting Comments on blog
+  it("should return comments of blog with route /blogs/blogId/likes", async () => {
+    const res = await request(app).get(
+      "/api/blogs/63d3e1635d04138e68e53c90/likes"
+    );
+    expect(res.body.status).toBe("success");
+  });
+
+  // // Getting a single blog
+  // test("Should return no blog found", async () => {
+  //   const res = await request(app).get("/api/blogs/63d3a1635d05138e68e53c90");
+  //   expect(res.body.message).toContain("Blog not found");
+  // }, 10000);
+
+  // // Invalid blog id
+  // test("Should return no blog if blog id is invalid", async () => {
+  //   const res = await request(app).get("/api/blogs/63d3e1635d05138e68 ");
+  //   expect(res.body.message).toContain("Invalid");
+  // }, 10000);
 });
