e0268b01144eaec97322da5d398f28231d6a6c4b
ericndungutse <dav.ndungutse@gmail.com>
2023-02-03 19:59:21 +0200

    feature(user): get user data
    - ensure user gets information about him
    - implement get me router
    
    [Delivers #63-user]

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -18,6 +18,28 @@ exports.updateMe = async (req, res, next) => {
   }
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user._id;
+  next();
+};
+
+exports.getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id).select(
+      "-password -_id -createdAt -updatedAt"
+    );
+
+    if (!user) return next(new AppError("User not found.", 400));
+
+    res.status(200).json({
+      status: "success",
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 exports.getAllUsers = async (req, res, next) => {
   try {
     const users = await User.find();
