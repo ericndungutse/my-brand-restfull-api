3bac0c1451025dbcf479d77537c2496990f72a4d
ericndungutse <dav.ndungutse@gmail.com>
2023-02-11 11:52:09 +0200

    chore(testing): fix bugs in tests

---
+++ tests/like.test.js
@@ -0,0 +1,64 @@
+const request = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../app");
+const { db } = require("./test.db.js");
+
+/* Connecting to the database before each test. */
+beforeAll(async () => {
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(`${db}`);
+});
+
+/* Closing database connection after each test. */
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoose.connection.close();
+});
+
+describe("Like CRUD", () => {
+  describe("CREATE", () => {
+    let token;
+
+    beforeAll(async () => {
+      process.env.JWT_SECRET = "secret-for-testing";
+      process.env.JWT_EXPIRES_IN = "1d";
+      const res = await request(app).post("/api/auth/login").send({
+        email: "bwiza@example.com",
+        password: "test12345",
+      });
+
+      token = res.body.token;
+    });
+
+    afterAll(() => {
+      process.env.JWT_SECRET = undefined;
+      process.env.JWT_EXPIRES_IN = undefined;
+    });
+
+    // Like created
+    it("should create like", async () => {
+      const like = await request(app)
+        .post("/api/likes")
+        .set("Authorization", "Bearer " + token)
+        .send({
+          blog: "63da1d145a3fda140ea0be4b",
+        });
+      expect(like.body.status).toBe("success");
+    }, 20000);
+
+    // Blog is missing
+    it("should not create like if blog is missing", async () => {
+      const like = await request(app)
+        .post("/api/likes")
+        .set("Authorization", "Bearer " + token)
+        .send({});
+      expect(like.body.status).toBe("fail");
+    }, 20000);
+  });
+
+  // Get all likes
+  it("should return all likes", async () => {
+    const likes = await request(app).get("/api/likes");
+    expect(likes.body.status).toBe("success");
+  }, 20000);
+});
